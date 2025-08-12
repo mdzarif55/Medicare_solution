d733567eca445abe74ffba048da32b5683058f5a
mdzarif55 <muhtasimzarif55@gmail.com>
2025-08-12 23:54:35 +0600

    working on backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,8 +3,6 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 
-
-
 // Register User /api/user/register
 
 export const register = async(req, res)=>{
@@ -37,4 +35,40 @@ export const register = async(req, res)=>{
         res.json({success:false , message: error.message}) 
     }
     
+}
+
+
+// LOGIN USER  : /API/USER/LOGIN
+
+export const login = async(req, res) =>{
+    try{
+        const {email, password} =req.body;
+        if(!email || !password)
+            return res.json({success: false, message:"Email and password are required"});
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.json({success: false, message:"Invalid email or password"});
+        }
+        
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch)
+            return res.json({success: false, message:"Invalid email or password"});
+        
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'})
+        
+        res.cookie('token', token, {
+            httpOnly: true,  // prevent javascript to access the cookie
+            secure: process.env.NODE_ENV === 'production', // Use secure cookie in production
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', //csrf protection 
+            maxAge: 7*24*60*60*1000, //cookie expire time
+        })
+
+        return res.json({success:true , user: {email:user.email, name:user.name}}) 
+
+
+    } catch(error){
+
+    }
 }
