3dce231e4c771b4d1caf795e05816a6a69fe7ef3
mdzarif55 <muhtasimzarif55@gmail.com>
2025-08-13 23:37:00 +0600

    all routes

---
+++ server/controllers/orderController.js
@@ -0,0 +1,35 @@
+import Order from "../models/Order.js";
+import Product from "../models/products.js";
+
+// placed or COD : /api/order/cod
+
+export const placeOrderCod = async (req, res) => {
+    try {
+        const { userId, items, address } = req.body;
+        if (!address || items.length == 0) {
+            return res.json({ success: false, message: "Indalid Date" })
+        }
+
+        //calculate amount using items
+        let amount = await items.reduce(async (acc, item) => {
+            const product = await Product.findById(item.product);
+            return (await acc) + product.offerPrice * item.quantity;
+        }, 0)
+
+        amount += Math.floor(amount * 0.02);
+
+        await Order.create({
+            userId,
+            items,
+            amount,
+            address,
+            paymentType: "COD"
+
+        });
+
+        return res.json({ success: true, message: "Order Placed Successfully" })
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message })
+    }
+}
--- server/server.js
+++ server/server.js
@@ -4,10 +4,17 @@ import cors from 'cors';
 import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import addressRouter from './routes/addressRoute.js';
+
 const app = express();
 const port = process.env.PORT || 4000;
 
 await connectDB()
+await connectCloudinary()
 
 //Allow Multiple origins
 const allowedOrgins = ['http://localhost:5173/']
@@ -20,6 +27,11 @@ app.use(cors({origin: allowedOrgins, credentials:true}));
 
 app.get('/', (req, res) =>  res.send("API is Working"));
 app.use('/api/user',userRouter)
+app.use('/api/seller',sellerRouter)
+app.use('/api/product',productRouter)
+app.use('/api/cart',cartRouter)
+app.use('/api/address',addressRouter)
+
 app.listen(port, () => {
   console.log(`✅ Server is running on http://localhost:${port}`);
 });
